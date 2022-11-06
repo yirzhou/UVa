a6931f00312a40391a105f42c955f9c5f813d5e3
yiren.chow@gmail.com <yiren.chow@gmail.com>
2022-11-06 08:25:21 -0800

    AC 104

--- Arbitrage_104.cpp
+++ Arbitrage_104.cpp
@@ -2,18 +2,29 @@
 
 using namespace std;
 
-#define N 20
+#define N 25
 int n;
 
 double dist[N][N][N];
 int path[N][N][N];
 
+void printfPath(int i, int j, int s)
+{
+    if (s == 0) {
+        printf("%d", i);
+        return;
+    }
+    printfPath(i, path[i][j][s], s - 1);
+    printf(" %d", j);
+    return;
+}
+
 void f()
 {
-    for (int l = 1; l < n; ++l) {
-        for (int k = 0; k < n; ++k) {
-            for (int i = 0; i < n; ++i) {
-                for (int j = 0; j < n; ++j) {
+    for (int l = 2; l <= n; ++l) {
+        for (int k = 1; k <= n; ++k) {
+            for (int i = 1; i <= n; ++i) {
+                for (int j = 1; j <= n; ++j) {
                     double d = dist[i][k][l - 1] * dist[k][j][1];
                     if (d > dist[i][j][l]) {
                         dist[i][j][l] = d;
@@ -23,20 +34,10 @@ void f()
             }
         }
 
-        for (int i = 0; i < n; ++i) {
+        for (int i = 1; i <= n; ++i) {
             if (dist[i][i][l] > 1.01) {
-                vector<int> s;
-                int j = i;
-                while (l - 1) {
-                    s.push_back(j = path[i][j][l--]);
-                }
-
-                printf("%d ", i + 1);
-                while (!s.empty()) {
-                    printf("%d ", s.back() + 1);
-                    s.pop_back();
-                }
-                printf("%d\n", i + 1);
+                printfPath(i, i, l);
+                printf("\n");
                 return;
             }
         }
@@ -47,21 +48,17 @@ void f()
 int main()
 {
     while (scanf("%d", &n) == 1) {
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < n; ++j) {
-                fill(dist[i][j], dist[i][j] + n, 0.0);
-                fill(path[i][j], path[i][j] + n, i);
-            }
-            fill(dist[i][i], dist[i][i] + n, 1.0);
-        }
+        memset(dist, 0, sizeof(dist));
+        memset(path, 0, sizeof(path));
 
-        for (int u = 0; u < n; ++u) {
-            for (int v = 0; v < n; ++v) {
-                if (v == u)
-                    continue;
-                double val;
-                scanf("%lf", &val);
-                fill(dist[u][v] + 1, dist[u][v] + n, val);
+        for (int u = 1; u <= n; ++u) {
+            for (int v = 1; v <= n; ++v) {
+                if (v == u) {
+                    dist[u][v][1] = 1.0;
+
+                } else {
+                    scanf("%lf", &dist[u][v][1]);
+                }
             }
         }
 
