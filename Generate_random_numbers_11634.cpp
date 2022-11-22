06178e59f1fe9607f82549656c3d6b4c37355ed6
yiren.chow@gmail.com <yiren.chow@gmail.com>
2022-11-21 23:32:51 -0800

    ac 11634

--- Generate_random_numbers_11634.cpp
+++ Generate_random_numbers_11634.cpp
@@ -20,8 +20,36 @@ typedef long long ll;
 typedef unsigned long long ull;
 typedef vector<ll> vll;
 
+int f(int a)
+{
+    return (a * a / 100) % 10000;
+}
+
+ii floyd_cf(int x0)
+{
+    int tortoise = f(x0), hare = f(f(x0));
+    while (tortoise != hare)
+        tortoise = f(tortoise), hare = f(f(hare));
+    int mu = 0;
+    hare = x0;
+    while (tortoise != hare)
+        tortoise = f(tortoise), hare = f(hare), mu++;
+
+    int lambda = 1;
+    hare = f(tortoise);
+    while (tortoise != hare)
+        hare = f(hare), lambda++;
+    return { mu, lambda };
+}
+
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+
+    int a0;
+    while (cin >> a0 && a0) {
+        ii res = floyd_cf(a0);
+        cout << res.fi + res.se << endl;
+    }
 }
