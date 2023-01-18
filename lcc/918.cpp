05c97669c0febda6bf76ef27fd96e7e734828417
Yiren Zhou <yiren.chow@gmail.com>
2023-01-17 20:47:31 -0800

    ac 918

---
+++ lcc/918.cpp
@@ -0,0 +1,93 @@
+#include <algorithm>
+#include <assert.h>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <math.h>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+#include <time.h>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+#define oo INT_MAX
+#define UNVISITED -1
+#define INF 1000000000
+#define EPS 1e-9
+#define pb push_back
+#define fi first
+#define se second
+
+#define ALL(v) v.begin(), v.end()
+#define pii(a, b) printf("%d %d\n", a, b)
+#define piii(a, b, c) printf("%d %d %d\n", a, b, c)
+#define pll(a, b) printf("%lld %lld\n", a, b)
+#define plll(a, b, c) printf("%lld %lld %lld\n", a, b, c)
+#define si(a) scanf("%d", &a)
+#define sii(a, b) scanf("%d%d", &a, &b)
+#define siii(a, b, c) scanf("%d%d%d", &a, &b, &c)
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0)
+#define precision(a) \
+    cout << fixed;   \
+    cout.precision(a)
+
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef vector<ll> vll;
+typedef vector<vi> vii;
+
+class Solution {
+public:
+    int f1(vector<int>& a)
+    {
+        int ans = a[0], sum = 0;
+        for (auto& num : a) {
+            sum += num;
+            ans = max(ans, sum);
+            sum = max(sum, 0);
+        }
+        return ans;
+    }
+
+    int f2(vector<int>& a)
+    {
+        int ans = a[0], sum = 0;
+        for (auto& num : a) {
+            sum += num;
+            ans = min(ans, sum);
+            sum = min(sum, 0);
+        }
+        return ans;
+    }
+
+    int maxSubarraySumCircular(vector<int>& nums)
+    {
+        int total = 0;
+        for (auto& num : nums)
+            total += num;
+        int max_sum = f1(nums), min_sum = f2(nums);
+        return max(max_sum, min_sum == total ? INT_MIN : total - min_sum);
+    }
+};
