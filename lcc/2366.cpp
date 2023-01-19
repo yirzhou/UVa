d0ae6dd81e90f630de7a6246b0cdf0d72cf7a635
Yiren Zhou <yiren.chow@gmail.com>
2023-01-19 00:11:11 -0800

    ac 2366

---
+++ lcc/2366.cpp
@@ -0,0 +1,105 @@
+#include <algorithm>
+#include <assert.h>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <math.h>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+#include <time.h>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+#define oo INT_MAX
+#define UNVISITED -1
+#define INF 1000000000
+#define EPS 1e-9
+#define pb push_back
+#define fi first
+#define se second
+
+#define ALL(v) v.begin(), v.end()
+#define pii(a, b) printf("%d %d\n", a, b)
+#define piii(a, b, c) printf("%d %d %d\n", a, b, c)
+#define pll(a, b) printf("%lld %lld\n", a, b)
+#define plll(a, b, c) printf("%lld %lld %lld\n", a, b, c)
+#define si(a) scanf("%d", &a)
+#define sii(a, b) scanf("%d%d", &a, &b)
+#define siii(a, b, c) scanf("%d%d%d", &a, &b, &c)
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0)
+#define precision(a) \
+    cout << fixed;   \
+    cout.precision(a)
+
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef vector<ll> vll;
+typedef vector<vi> vii;
+
+/*
+You are given a 0-indexed integer array nums. In one operation you can replace any element of the array with any two elements that sum to it.
+
+For example, consider nums = [5,6,7]. In one operation, we can replace nums[1] with 2 and 4 and convert nums to [5,2,4,7].
+Return the minimum number of operations to make an array that is sorted in non-decreasing order.
+
+Input: nums = [3,9,3]
+Output: 2
+Explanation: Here are the steps to sort the array in non-decreasing order:
+- From [3,9,3], replace the 9 with 3 and 6 so the array becomes [3,3,6,3]
+- From [3,3,6,3], replace the 6 with 3 and 3 so the array becomes [3,3,3,3,3]
+There are 2 steps to sort the array in non-decreasing order. Therefore, we return 2.
+*/
+
+class Solution {
+public:
+    long long minimumReplacement(vector<int>& nums)
+    {
+        long long ans = 0;
+        int n = nums.size();
+        ll prev = nums.back();
+        for (int i = n - 2; i >= 0; --i) {
+            ll cur = nums[i];
+            int k = ceil((double)cur / prev);
+            ans += k - 1;
+            prev = cur / k;
+        }
+
+        return ans;
+    }
+};
+
+int main()
+{
+    int t, n;
+    cin >> t;
+    while (t--) {
+        cin >> n;
+        vi nums(n);
+        for (int i = 0; i < n; ++i)
+            cin >> nums[i];
+        Solution sol;
+        cout << sol.minimumReplacement(nums) << endl;
+    }
+}
