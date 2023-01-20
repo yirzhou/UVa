03518c47fcd2e7d21ca8b3bab90b3a458098877e
Yiren Zhou <yiren.chow@gmail.com>
2023-01-19 22:14:22 -0800

    ac 2065

--- lcc/2065.cpp
+++ lcc/2065.cpp
@@ -60,7 +60,81 @@ typedef vector<vi> vii;
 
 class Solution {
 public:
+    int n;
+    vector<unordered_map<int, int>> AL;
+    map<ii, int> cnt;
+    vi vals;
+    unordered_map<int, int> S;
+
+    int ans = 0;
+
+    void f(int u, int quality, int tleft)
+    {
+        if (tleft < 0)
+            return;
+
+        if (S.find(u) == S.end())
+            quality += vals[u];
+        if (u == 0)
+            ans = max(ans, quality);
+        S[u]++;
+
+        for (auto& [v, cost] : AL[u]) {
+            ii E = { u, v };
+            if (cnt[E] == 1)
+                cnt[E]--, f(v, quality, tleft - AL[u][v]), cnt[E]++;
+        }
+        S[u]--;
+        if (S[u] == 0)
+            S.erase(u);
+    }
+
     int maximalPathQuality(vector<int>& values, vector<vector<int>>& edges, int maxTime)
     {
+        n = values.size();
+        vals = values;
+        S.clear();
+        AL.assign(n, unordered_map<int, int>());
+        for (auto& e : edges) {
+            int u = e[0], v = e[1], time = e[2];
+            cnt[{ u, v }] = cnt[{ v, u }] = 1;
+            AL[u][v] = AL[v][u] = time;
+        }
+        f(0, 0, maxTime);
+
+        return ans;
     }
 };
+
+int main()
+{
+    int t, sz, edge_cnt, max_time;
+    cin >> t;
+    while (t--) {
+        cin >> sz;
+        vi values(sz);
+        for (int i = 0; i < sz; ++i)
+            cin >> values[i];
+
+        cin >> edge_cnt;
+        vii edges;
+        int u, v, time;
+        for (int i = 0; i < edge_cnt; ++i)
+            cin >> u >> v >> time, edges.pb({ u, v, time });
+        cin >> max_time;
+
+        Solution sol;
+        int ans = sol.maximalPathQuality(values, edges, max_time);
+        cout << ans << endl;
+    }
+}
+
+/*
+There is an undirected graph with n nodes numbered from 0 to n - 1 (inclusive). You are given a 0-indexed integer array values where values[i] is the value of the ith node. You are also given a 0-indexed 2D integer array edges, where each edges[j] = [uj, vj, timej] indicates that there is an undirected edge between the nodes uj and vj, and it takes timej seconds to travel between the two nodes. Finally, you are given an integer maxTime.
+
+A valid path in the graph is any path that starts at node 0, ends at node 0, and takes at most maxTime seconds to complete. You may visit the same node multiple times. The quality of a valid path is the sum of the values of the unique nodes visited in the path (each node's value is added at most once to the sum).
+
+Return the maximum quality of a valid path.
+
+Note: There are at most four edges connected to each node.
+*/
