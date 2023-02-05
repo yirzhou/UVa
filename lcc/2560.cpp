47c867df67723a2132d710996e9d3f28a3c531be
Yiren Zhou <yiren.chow@gmail.com>
2023-02-04 21:41:38 -0800

    2560

---
+++ lcc/2560.cpp
@@ -0,0 +1,86 @@
+#include <algorithm>
+#include <assert.h>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <math.h>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+#include <time.h>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+#define oo INT_MAX
+#define UNVISITED -1
+#define INF 1000000000
+#define EPS 1e-9
+#define pb push_back
+#define fi first
+#define se second
+#define MOD 1000000007
+
+#define ALL(v) v.begin(), v.end()
+#define pii(a, b) printf("%d %d\n", a, b)
+#define piii(a, b, c) printf("%d %d %d\n", a, b, c)
+#define pll(a, b) printf("%lld %lld\n", a, b)
+#define plll(a, b, c) printf("%lld %lld %lld\n", a, b, c)
+#define si(a) scanf("%d", &a)
+#define sii(a, b) scanf("%d%d", &a, &b)
+#define siii(a, b, c) scanf("%d%d%d", &a, &b, &c)
+#define fastio                                                                 \
+    ios_base::sync_with_stdio(false);                                          \
+    cin.tie(0)
+#define precision(a)                                                           \
+    cout << fixed;                                                             \
+    cout.precision(a)
+
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+typedef vector<vi> vvi;
+
+class Solution {
+  public:
+    int f(vector<int> &nums, int k, int target) {
+        vector<int> S;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] <= target && !S.back() != i - 1) S.pb(i);
+            if (S.size() == k) return 1;
+        }
+        return 0;
+    }
+
+    int minCapability(vector<int> &nums, int k) {
+        int lo = 1, hi = *max_element(ALL(nums));
+        hi++;
+
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (f(nums, k, mid)) hi = mid;
+            else lo = mid + 1;
+        }
+
+        return lo;
+    }
+};
