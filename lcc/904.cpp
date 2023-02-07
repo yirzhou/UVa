f5782d12bfcbbbdb60b19e5ae45ce9cf898b047f
Yiren Zhou <yiren.chow@gmail.com>
2023-02-06 21:21:28 -0800

    ac 904

---
+++ lcc/904.cpp
@@ -0,0 +1,79 @@
+#include <algorithm>
+#include <assert.h>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <math.h>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+#include <time.h>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+#define oo INT_MAX
+#define UNVISITED -1
+#define INF 1000000000
+#define EPS 1e-9
+#define pb push_back
+#define fi first
+#define se second
+#define MOD 1000000007
+
+#define ALL(v) v.begin(), v.end()
+#define pii(a, b) printf("%d %d\n", a, b)
+#define piii(a, b, c) printf("%d %d %d\n", a, b, c)
+#define pll(a, b) printf("%lld %lld\n", a, b)
+#define plll(a, b, c) printf("%lld %lld %lld\n", a, b, c)
+#define si(a) scanf("%d", &a)
+#define sii(a, b) scanf("%d%d", &a, &b)
+#define siii(a, b, c) scanf("%d%d%d", &a, &b, &c)
+#define fastio                                                                 \
+    ios_base::sync_with_stdio(false);                                          \
+    cin.tie(0)
+#define precision(a)                                                           \
+    cout << fixed;                                                             \
+    cout.precision(a)
+
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+typedef vector<vi> vvi;
+
+class Solution {
+  public:
+    int totalFruit(vector<int> &fruits) {
+        unordered_map<int, int> mp;
+        int i = 0, ans = 0;
+        for (int j = 0; j < fruits.size(); ++j) {
+            int num = fruits[j];
+            mp[num]++;
+            while (mp.size() > 2) {
+                int rm = fruits[i++];
+                if (--mp[rm] == 0) mp.erase(rm);
+            }
+            ans = max(ans, j - i + 1);
+        }
+        return ans;
+    }
+};
