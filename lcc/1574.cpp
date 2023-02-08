4b9ca5734c6d477b65e85ea23fd40228e4a605e3
Yiren Zhou <yiren.chow@gmail.com>
2023-02-08 08:03:28 -0800

    ac 1574

---
+++ lcc/1574.cpp
@@ -0,0 +1,94 @@
+#include <algorithm>
+#include <assert.h>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <math.h>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+#include <time.h>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+#define oo INT_MAX
+#define UNVISITED -1
+#define INF 1000000000
+#define EPS 1e-9
+#define pb push_back
+#define fi first
+#define se second
+#define MOD 1000000007
+
+#define ALL(v) v.begin(), v.end()
+#define pii(a, b) printf("%d %d\n", a, b)
+#define piii(a, b, c) printf("%d %d %d\n", a, b, c)
+#define pll(a, b) printf("%lld %lld\n", a, b)
+#define plll(a, b, c) printf("%lld %lld %lld\n", a, b, c)
+#define si(a) scanf("%d", &a)
+#define sii(a, b) scanf("%d%d", &a, &b)
+#define siii(a, b, c) scanf("%d%d%d", &a, &b, &c)
+#define fastio                                                                 \
+    ios_base::sync_with_stdio(false);                                          \
+    cin.tie(0)
+#define precision(a)                                                           \
+    cout << fixed;                                                             \
+    cout.precision(a)
+
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+typedef vector<vi> vvi;
+
+/*
+- 1 <= arr.length <= 10^5
+- 0 <= arr[i] <= 10^9
+*/
+class Solution {
+  public:
+    int findLengthOfShortestSubarray(vector<int> &arr) {
+        int n = arr.size();
+        int lend = 0, rstart = n - 1;
+        while (lend + 1 < arr.size() && arr[lend] <= arr[lend + 1]) lend++;
+        while (rstart > 0 && arr[rstart] >= arr[rstart - 1]) rstart--;
+        if (lend >= rstart) return 0;
+        if (arr[lend] <= arr[rstart]) return rstart - lend - 1;
+        int ans = n;
+
+        for (int i = 0; i <= lend; ++i) {
+            int num = arr[i];
+            int iter =
+                lower_bound(arr.begin() + rstart, arr.end(), num) - arr.begin();
+            ans = min(ans, iter - i - 1);
+        }
+
+        for (int i = rstart; i < n; ++i) {
+            int num = arr[i];
+            int iter =
+                lower_bound(arr.begin(), arr.begin() + lend, num) - arr.begin();
+            ans = min(ans, i - iter);
+        }
+
+        return ans;
+    }
+};
