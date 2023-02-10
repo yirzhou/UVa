4b5779e01de3332a8e573fab4628920360686b9f
Yiren Zhou <yiren.chow@gmail.com>
2023-02-10 07:12:45 -0800

    ac 1552

---
+++ lcc/1552.cpp
@@ -0,0 +1,97 @@
+#include <algorithm>
+#include <assert.h>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <math.h>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+#include <time.h>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+#define oo INT_MAX
+#define UNVISITED -1
+#define INF 1000000000
+#define EPS 1e-9
+#define pb push_back
+#define fi first
+#define se second
+#define MOD 1000000007
+
+#define ALL(v) v.begin(), v.end()
+#define pii(a, b) printf("%d %d\n", a, b)
+#define piii(a, b, c) printf("%d %d %d\n", a, b, c)
+#define pll(a, b) printf("%lld %lld\n", a, b)
+#define plll(a, b, c) printf("%lld %lld %lld\n", a, b, c)
+#define si(a) scanf("%d", &a)
+#define sii(a, b) scanf("%d%d", &a, &b)
+#define siii(a, b, c) scanf("%d%d%d", &a, &b, &c)
+#define fastio                                                                 \
+    ios_base::sync_with_stdio(false);                                          \
+    cin.tie(0)
+#define precision(a)                                                           \
+    cout << fixed;                                                             \
+    cout.precision(a)
+
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+typedef vector<vi> vvi;
+
+/*
+n == position.length
+2 <= n <= 10^5
+1 <= position[i] <= 10^9
+All integers in position are distinct.
+2 <= m <= position.length
+*/
+class Solution {
+  public:
+    int n;
+    int good(vi &A, int m, ll k) {
+        ll prev = A[0];
+        m -= 1;
+        for (int i = 1; i < n && m > 0; ++i) {
+            ll cur = A[i];
+            if (prev + k > cur) continue;
+            prev = cur, m--;
+        }
+
+        return m == 0;
+    }
+    int maxDistance(vector<int> &A, int m) {
+        n = A.size();
+        sort(ALL(A));
+
+        ll lo = 1, hi = A.back() - A.front() + 1;
+        while (lo < hi) {
+            ll mid = lo + (hi - lo) / 2;
+            if (good(A, m, mid)) lo = mid + 1;
+            else hi = mid;
+        }
+
+        return lo - 1;
+    }
+};
