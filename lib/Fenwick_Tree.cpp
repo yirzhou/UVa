133210a26ad40c1f0393651db0bbb2301f611ebd
Yiren Zhou <yiren.chow@gmail.com>
2023-02-12 15:41:54 -0800

    edit fenwick tree

--- lib/Fenwick_Tree.cpp
+++ lib/Fenwick_Tree.cpp
@@ -1,13 +1,40 @@
-#include <bits/stdc++.h>
+#include <algorithm>
+#include <assert.h>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <math.h>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+#include <time.h>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
 
 using namespace std;
 
 #define oo INT_MAX
+#define UNVISITED -1
 #define INF 1000000000
 #define EPS 1e-9
 #define pb push_back
 #define fi first
 #define se second
+#define MOD 1000000007
 
 #define ALL(v) v.begin(), v.end()
 #define pii(a, b) printf("%d %d\n", a, b)
@@ -17,11 +44,11 @@ using namespace std;
 #define si(a) scanf("%d", &a)
 #define sii(a, b) scanf("%d%d", &a, &b)
 #define siii(a, b, c) scanf("%d%d%d", &a, &b, &c)
-#define fastio                        \
-    ios_base::sync_with_stdio(false); \
+#define fastio                                                                 \
+    ios_base::sync_with_stdio(false);                                          \
     cin.tie(0)
-#define precision(a) \
-    cout << fixed;   \
+#define precision(a)                                                           \
+    cout << fixed;                                                             \
     cout.precision(a)
 
 typedef vector<int> vi;
@@ -29,59 +56,49 @@ typedef pair<int, int> ii;
 typedef long long ll;
 typedef unsigned long long ull;
 typedef long double ld;
-typedef vector<ll> vll;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+typedef vector<vi> vvi;
 
 #define LSOne(S) ((S) & -(S))
 
 class FenwickTree {
-private:
-    vll ft;
+  private:
+    vl ft;
 
-public:
+  public:
     FenwickTree(int m) { ft.assign(m + 1, 0); }
 
-    FenwickTree(const vll& f) { build(f); }
+    FenwickTree(const vl &f) { build(f); }
 
-    FenwickTree(int m, const vll& s)
-    {
-        vll f(m + 1, 0);
-        for (int i = 0; i < s.size(); ++i)
-            ++f[s[i]];
+    FenwickTree(int m, const vl &s) {
+        vl f(m + 1, 0);
+        for (int i = 0; i < s.size(); ++i) ++f[s[i]];
         build(f);
     }
 
-    void build(const vll& f)
-    {
+    void build(const vl &f) {
         int m = (int)f.size() - 1;
         ft.assign(m + 1, 0);
         for (int i = 1; i <= m; ++i) {
             ft[i] += f[i];
-            if (i + LSOne(i) <= m)
-                ft[i + LSOne(i)] += ft[i];
+            if (i + LSOne(i) <= m) ft[i + LSOne(i)] += ft[i];
         }
     }
 
-    int rsq(int j)
-    {
+    int rsq(int j) {
         int sum = 0;
-        for (; j; j -= LSOne(j))
-            sum += ft[j];
+        for (; j; j -= LSOne(j)) sum += ft[j];
         return sum;
     }
 
-    int rsq(int i, int j)
-    {
-        return rsq(j) - rsq(i - 1);
+    int rsq(int i, int j) { return rsq(j) - rsq(i - 1); }
+
+    void update(int i, int j) {
+        for (; i < (int)ft.size(); i += LSOne(i)) ft[i] += j;
     }
 
-    void update(int i, int j)
-    {
-        for (; i < (int)ft.size(); i += LSOne(i))
-            ft[i] += j;
-    }
-
-    int select(ll k)
-    {
+    int select(ll k) {
         int lo = 1, hi = ft.size() - 1;
         for (int i = -0; i < 30; ++i) {
             int mid = (lo + hi) / 2;
@@ -92,14 +109,12 @@ public:
 };
 
 class RUPQ {
-private:
+  private:
     FenwickTree ft;
 
-public:
-    RUPQ(int m)
-        : ft(FenwickTree(m)) {};
-    void range_update(int ui, int uj, int v)
-    {
+  public:
+    RUPQ(int m) : ft(FenwickTree(m)){};
+    void range_update(int ui, int uj, int v) {
         ft.update(ui, v);
         ft.update(uj + 1, -v);
     }
@@ -108,31 +123,20 @@ public:
 };
 
 class RURQ {
-private:
+  private:
     RUPQ rupq;
     FenwickTree purq;
 
-public:
-    RURQ(int m)
-        : rupq(RUPQ(m))
-        , purq(FenwickTree(m))
-    {
-    }
+  public:
+    RURQ(int m) : rupq(RUPQ(m)), purq(FenwickTree(m)) {}
 
-    void range_update(int ui, int uj, int v)
-    {
+    void range_update(int ui, int uj, int v) {
         rupq.range_update(ui, uj, v);
         purq.update(ui, v * (ui - 1));
         purq.update(uj + 1, -v * uj);
     }
 
-    ll rsq(int j)
-    {
-        return rupq.point_query(j) * j - purq.rsq(j);
-    }
+    ll rsq(int j) { return rupq.point_query(j) * j - purq.rsq(j); }
 
-    ll rsq(int i, int j)
-    {
-        return rsq(j) - rsq(i - 1);
-    }
+    ll rsq(int i, int j) { return rsq(j) - rsq(i - 1); }
 };
