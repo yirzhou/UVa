8c23a1c718e65e35034c2da207818bc25c344b76
Yiren Zhou <yiren.chow@gmail.com>
2023-02-13 07:36:00 -0800

    ac 2563

---
+++ lcc/2563.cpp
@@ -0,0 +1,80 @@
+#include <algorithm>
+#include <assert.h>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <math.h>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+#include <time.h>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+#define oo INT_MAX
+#define UNVISITED -1
+#define INF 1000000000
+#define EPS 1e-9
+#define pb push_back
+#define fi first
+#define se second
+#define MOD 1000000007
+
+#define ALL(v) v.begin(), v.end()
+#define pii(a, b) printf("%d %d\n", a, b)
+#define piii(a, b, c) printf("%d %d %d\n", a, b, c)
+#define pll(a, b) printf("%lld %lld\n", a, b)
+#define plll(a, b, c) printf("%lld %lld %lld\n", a, b, c)
+#define si(a) scanf("%d", &a)
+#define sii(a, b) scanf("%d%d", &a, &b)
+#define siii(a, b, c) scanf("%d%d%d", &a, &b, &c)
+#define fastio                                                                 \
+    ios_base::sync_with_stdio(false);                                          \
+    cin.tie(0)
+#define precision(a)                                                           \
+    cout << fixed;                                                             \
+    cout.precision(a)
+
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+typedef vector<vi> vvi;
+
+class Solution {
+  public:
+    long long countFairPairs(vector<int> &nums, int lower, int upper) {
+        long long ans = 0;
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size(); ++i) {
+            int num = nums[i];
+            auto i1 =
+                     lower_bound(nums.begin() + i + 1, nums.end(), lower - num),
+                 i2 = lower_bound(nums.begin() + i + 1, nums.end(),
+                                  upper - num + 1);
+            if (i1 == nums.end()) continue;
+            long long p1 = i1 - nums.begin(), p2 = i2 - nums.begin();
+            ans += max(0LL, p2 - p1);
+        }
+        return ans;
+    }
+};
