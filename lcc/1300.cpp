d85211427c6d0bbd036b08dbe4861d9a0f4215a7
Yiren Zhou <yiren.chow@gmail.com>
2023-02-13 07:49:47 -0800

    ac 1300

---
+++ lcc/1300.cpp
@@ -0,0 +1,92 @@
+#include <algorithm>
+#include <assert.h>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <math.h>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+#include <time.h>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+#define oo INT_MAX
+#define UNVISITED -1
+#define INF 1000000000
+#define EPS 1e-9
+#define pb push_back
+#define fi first
+#define se second
+#define MOD 1000000007
+
+#define ALL(v) v.begin(), v.end()
+#define pii(a, b) printf("%d %d\n", a, b)
+#define piii(a, b, c) printf("%d %d %d\n", a, b, c)
+#define pll(a, b) printf("%lld %lld\n", a, b)
+#define plll(a, b, c) printf("%lld %lld %lld\n", a, b, c)
+#define si(a) scanf("%d", &a)
+#define sii(a, b) scanf("%d%d", &a, &b)
+#define siii(a, b, c) scanf("%d%d%d", &a, &b, &c)
+#define fastio                                                                 \
+    ios_base::sync_with_stdio(false);                                          \
+    cin.tie(0)
+#define precision(a)                                                           \
+    cout << fixed;                                                             \
+    cout.precision(a)
+
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+typedef vector<vi> vvi;
+
+/*
+Input: arr = [4,9,3], target = 10
+Output: 3
+Explanation: When using 3 arr converts to [3, 3, 3] which sums 9 and that's the
+optimal answer.
+*/
+class Solution {
+  public:
+    int f(vi &nums, int target) {
+        int sum = 0;
+        for (auto &num : nums) sum += min(target, num);
+        return sum;
+    }
+
+    int findBestValue(vector<int> &arr, int target) {
+        int sum = 0;
+        for (auto &num : arr) sum += num;
+        if (sum < target) return *max_element(ALL(arr));
+        int lo = 0, hi = 1e5 + 1;
+        while (lo < hi) {
+            int mid = (lo + hi) / 2;
+            int res = f(arr, mid);
+            if (res < target) lo = mid + 1;
+            else hi = mid;
+        }
+        int r1 = f(arr, lo - 1), r2 = f(arr, lo);
+        if (abs(r1 - target) <= abs(r2 - target)) return lo - 1;
+        return lo;
+    }
+};
