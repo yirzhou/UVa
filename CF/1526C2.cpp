89df5545578a538869e2d997fc2b195e92d2f24e
Yiren Zhou <yiren.chow@gmail.com>
2023-02-27 08:42:54 -0800

    ac 1562

---
+++ CF/1526C2.cpp
@@ -0,0 +1,81 @@
+#include <algorithm>
+#include <assert.h>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <math.h>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+#include <time.h>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+#define oo INT_MAX
+#define UNVISITED -1
+#define INF 1000000000
+#define EPS 1e-9
+#define pb push_back
+#define fi first
+#define se second
+#define MOD 1000000007
+
+#define ALL(v) v.begin(), v.end()
+#define pii(a, b) printf("%d %d\n", a, b)
+#define piii(a, b, c) printf("%d %d %d\n", a, b, c)
+#define pll(a, b) printf("%lld %lld\n", a, b)
+#define plll(a, b, c) printf("%lld %lld %lld\n", a, b, c)
+#define si(a) scanf("%d", &a)
+#define sii(a, b) scanf("%d%d", &a, &b)
+#define siii(a, b, c) scanf("%d%d%d", &a, &b, &c)
+#define fastio                                                                 \
+    ios_base::sync_with_stdio(false);                                          \
+    cin.tie(0)
+#define precision(a)                                                           \
+    cout << fixed;                                                             \
+    cout.precision(a)
+
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+typedef vector<vi> vvi;
+
+int main() {
+    fastio;
+    int n;
+    cin >> n;
+    ll S = 0;
+    ll p;
+    priority_queue<ll, vector<ll>, greater<ll>> pq;
+    for (int i = 0; i < n; ++i) {
+        cin >> p;
+        S += p;
+        pq.push(p);
+        while (S < 0) {
+            S -= pq.top();
+            pq.pop();
+        }
+    }
+
+    cout << pq.size() << endl;
+}
--- CF/README.md
+++ CF/README.md
@@ -8,3 +8,5 @@ My solutions to some CF problems.
 
 - 1336A
 - 1398C
+- 1352G
+- **1526C** (very simple and beautiful solution in the end)
